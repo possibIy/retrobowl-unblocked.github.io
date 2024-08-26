c24f6dca05dcf62b5c886bd9af73da75e3062f7b
gracehuynhh <gracehuynh98@gmail.com>
2024-08-26 17:47:29 +0700

    ad

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* --------------------------------------------------------
+            34. scrollUp active
+        -------------------------------------------------------- */
+$.scrollUp({
+    scrollText: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12 2l-10 10h6v10h8v-10h6z"/></svg>',
+
+    easingType: 'linear',
+    scrollSpeed: 900,
+    animation: 'fade'
+});
